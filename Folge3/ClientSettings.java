039048931cc3fb26eebdfec27002013402a16ce3
shinigami <60923863+yaquza@users.noreply.github.com>
2021-01-10 22:19:09 +0100

    Add files via upload

---
+++ Folge3/ClientSettings.java
@@ -0,0 +1,9 @@
+package de.yaquza.client;
+
+public class ClientSettings {
+
+   public static boolean FPS = false;
+   public static boolean Ping = false;
+   public static boolean Coords = false;
+
+}
